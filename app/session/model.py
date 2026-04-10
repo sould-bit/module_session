4675dbc27cb06edc7dc094f4e96487add203be57
sould-bit <jnonstiiven@gmail.com>
2026-04-10 09:57:17 -0500

    cambiamos el nombre de la calse para no entrar en confucion , se agrego un id a la session de un usuario

--- app/session/model.py
+++ app/session/model.py
@@ -1,9 +1,9 @@
-from dataclasses import field
-from datetime import date, datetime
+from datetime import datetime
 from sqlmodel import Field, SQLModel  
 
 #entidad session
-class Session(SQLModel, table=True):
+class User_session(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     usuario_id: int = Field(foreign_key="user.id")
     date_inicio : datetime =Field(default_factory= datetime.now)
     date_fin : datetime = Field(default_factory= datetime.now)
