f8dbd1552b98b7b65083b76696ec5ec3ba4a7388
sould-bit <jnonstiiven@gmail.com>
2026-04-10 09:52:55 -0500

    se modifico   el campo de los atributos  de la entidad  , se quito autoincrement , por default none , se quito un aintencion de validacion con regex , los modelos no debe de saber de validaciones , solo les interesa la estructura de los datos para separar responsabilidades

--- app/register/model.py
+++ app/register/model.py
@@ -1,8 +1,8 @@
-from sqlmodel import Field, SQLModel  
+from sqlmodel import Field, SQLModel 
+from pydantic import field_validator
 
 #entidad usuario
 class User(SQLModel, table=True):
-    id: int = Field(autoincrement=True, primary_key=True)
+    id: int = Field(default=None, primary_key=True)
     name: str =Field(min_length=4, max_length=50)
-    password: str =Field(min_length=8, regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
-
+    password: str =Field(min_length=8)
