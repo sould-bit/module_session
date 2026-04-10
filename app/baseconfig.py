b6e99ae66c91638b7be162c539a540742fd08fea
sould-bit <jnonstiiven@gmail.com>
2026-04-10 09:44:51 -0500

    creamos una funcion , que pueda ejecutar la creacion de la base de datos

--- app/baseconfig.py
+++ app/baseconfig.py
@@ -1,4 +1,10 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, SQLModel
 
+from app.register.model import User
+from app.session.model import User_session
 
+#creamos la maquina engine 
 engine = create_engine("sqlite:///database_test.db")
+
+def create_db_and_table():
+    SQLModel.metadata.create_all(engine)
